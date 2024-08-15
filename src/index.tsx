5c33caa9c50b4450ffef2995a2d2203e7de43d86
Aky <Aky@aky.aky>
2024-08-15 21:26:30 +0900

    Create History View using Router and Paging

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { createGlobalStyle } from "styled-components";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
 
 import { Editor } from "./pages/editor";
+import { History } from "./pages/history";
+import { useStateWithStorage } from "./hooks/use_state_with_storage";
 
 const GlobalStyle = createGlobalStyle`
     body * {
@@ -10,11 +18,31 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
-const Main = (
-    <>
-        <GlobalStyle />
-        <Editor />
-    </>
-);
+const InitialString = "";
+const StorageKey = "/editor:text";
 
-render(Main, document.getElementById("app"));
+const Main: React.FC = () => {
+    const [text, setText] = useStateWithStorage(InitialString, StorageKey);
+
+    return (
+        <>
+            <GlobalStyle />
+            <Router>
+                <Route exact path="/editor">
+                    <Editor
+                        text={text}
+                        setText={setText}
+                    />
+                </Route>
+                <Route exact path="/history">
+                    <History
+                        setText={setText}
+                    />
+                </Route>
+                <Redirect to="/editor" path="*" />
+            </Router>
+        </>
+    );
+};
+
+render(<Main />, document.getElementById("app"));
--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -1,7 +1,7 @@
 import Dexie from "dexie";
 
 // TypeScript の型定義であって IndexedDB に対する設定ではない
-export interface MemoRecords {
+export interface MemoRecord {
     datetime: string
     title: string
     text: string
@@ -13,10 +13,28 @@ const database = new Dexie("markdown-editor");
 database.version(1).stores({ memos: "&datetime" });
 
 // Table<TypeOfData, TypeOfKey>
-const memos: Dexie.Table<MemoRecords, string> = database.table("memos");
+const memos: Dexie.Table<MemoRecord, string> = database.table("memos");
 
 export const putMemo = async (title: string, text: string): Promise<void> => {
     const datetime = new Date().toISOString();
 
     await memos.put({datetime, title, text});
+};
+
+const NUM_PER_PAGE: number = 10;
+
+export const getMemoPageCount = async (): Promise<number> => {
+    const totalCount = await memos.count();
+    const pageCount = Math.ceil(totalCount / NUM_PER_PAGE);
+    return Math.max(pageCount, 1);
+};
+
+export const getMemos = (page: number): Promise<MemoRecord[]> => {
+    const offset = (page - 1) * NUM_PER_PAGE;
+
+    return memos.orderBy("datetime")
+        .reverse()
+        .offset(offset)
+        .limit(NUM_PER_PAGE)
+        .toArray()
 };
